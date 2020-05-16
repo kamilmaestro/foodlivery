87e2a81e81b70d3431a441a3b19e59b369a7ec72
kamilmaestro <ksdekamil@gmail.com>
2020-05-16 21:46:45 +0200

    init commit

---
+++ src/main/java/com/kamilmarnik/foodlivery/FoodliveryApplication.java
@@ -0,0 +1,13 @@
+package com.kamilmarnik.foodlivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamilmarnik/foodlivery/FoodliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamilmarnik.foodlivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
